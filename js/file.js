4ff19f93a032d0a128c0b79c3d7eb6603142991d
devnikop <petrov.petrov.ak@gmail.com>
2020-03-10 16:56:43 +0300

    add file reader

---
+++ js/file.js
@@ -0,0 +1,45 @@
+(function() {
+  var FILE_TYPES = ["gif", "jpg", "jpeg", "png"];
+
+  var getElement = {
+    Selector: {
+      AD_FORM_HEADER: `.ad-form-header`,
+      FORM_FILE_PREVIEW: `.ad-form-header__preview img`,
+      FORM_INPUT_FILE: `.ad-form__field input`
+    },
+
+    get adFormHeader() {
+      return document.querySelector(this.Selector.AD_FORM_HEADER);
+    },
+
+    get filePreview() {
+      return this.adFormHeader.querySelector(this.Selector.FORM_FILE_PREVIEW);
+    },
+
+    get formInputFile() {
+      return this.adFormHeader.querySelector(this.Selector.FORM_INPUT_FILE);
+    }
+  };
+
+  var fileChooser = getElement.formInputFile;
+  var preview = getElement.filePreview;
+
+  fileChooser.addEventListener(`change`, function() {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function(it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener("load", function() {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
